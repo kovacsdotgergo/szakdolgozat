72b3d2b60b6261c26c8c296595cb9dccb417453d
gergo <gergo@Gergo-PC.localdomain>
2022-11-17 11:01:11 +0100

    intitial commit

---
+++ esc_dataset.py
@@ -0,0 +1,216 @@
+import torch
+from torch.utils.data import Dataset
+import torch.nn as nn
+
+from torchvision.transforms import Normalize
+
+import torchaudio
+import numpy as np
+import pandas as pd
+
+#constants for augmentation
+MAX_NOISE_FACTOR = 0.03
+MAX_SHIFT = 0.5
+MAX_PITCH_SHIFT_STEP = 6
+#constants for normalization, calculated on the whole dataset
+DATASET_KALDI_MEAN = -6.773274898529053
+DATASET_KALDI_STD = 5.422067642211914
+DATASET_RAW_MEAN = -0.00014325266238301992
+DATASET_RAW_STD = 0.12926168739795685
+DATASET_MEAN = -51.28519821166992
+DATASET_STD = 46.351680755615234
+
+
+class ESCdataset(Dataset):
+    def __init__(self, path, n_fft=1024, hop_length=512, n_mels=128,
+                augment=True, pitch_shift=False, normalize=True,
+                log_mel=True, use_kaldi=True, target_len = None,
+                resample_rate=None) -> None:
+        """
+        @brief  ESC-50 dataset class
+
+        @param[in]  augment     bool, should data augmentation be used
+        @param[in]  pitch_shift bool, should pitch shift augmentation be used
+        @param[in]  normalize   bool, should normalization be used
+        @param[in]  log_mel     bool, should logmel transformation be used
+        @param[in]  use_kaldi   bool, should kaldi.fbank be used for logmel
+        @param[in]  target_len  int, target len for padding
+        @param[in]  n_fft, hop_length, n_mels parameters for logarithmic Mel spectogram
+        """
+        super().__init__()
+        self.path = path
+        self.n_fft = n_fft
+        self.hop_length = hop_length
+        self.n_mels = n_mels
+        self.augment = augment
+        self.pitch_shift = pitch_shift
+        self.normalize = normalize
+        self.log_mel = log_mel
+        self.use_kaldi = use_kaldi
+        self.target_len = target_len
+
+        #reading the csv file for creating labels
+        alldata = pd.read_csv(path + "/meta/esc50.csv").to_numpy()
+        #filename and the index/code of the category
+        self.csv_data = alldata[:, [0, 2]]
+        #dictionary for the indexes and category names
+        self.label_dict = dict(alldata[:, [2, 3]])
+
+        #all files should have the same sample rate, saving based on the first file
+        _, original_sr = torchaudio.load(
+            self.path + "/audio/" + self.csv_data[0, 0])
+        self.sample_rate = original_sr if resample_rate is None else resample_rate
+
+        #instantiating the classes for transfoming the data
+        self.Resample = self._create_resample(original_sr, self.sample_rate)
+        self.LogMelSpect = self._create_log_mel_spect()
+        self.Normalize = self._create_normalize()
+
+    def __len__(self) -> int:
+        """@returns the length of the dataset"""
+        return len(self.csv_data)
+
+    def __getitem__(self, item: int):
+        """@returns (data, label)        
+        @param[in]  item    index in the csv file
+        """
+        data = self._get_data(item)
+        label = self._get_label(item)
+        return data, label
+
+    def _get_label(self, item: int):
+        """@returns label based on the index"""
+        return self.csv_data[item, 1]
+    
+    #TODO: function for returning class name from dict given the index
+
+    def _get_data(self, item: int):
+        """@returns the transformed data based on the index
+        @param[in]  item    index in the csv file
+        """
+        data = self.get_waveform(item)
+        data = self.Resample(data)
+        if self.augment:
+            data = self.transform(data, MAX_SHIFT, MAX_PITCH_SHIFT_STEP,
+                                MAX_NOISE_FACTOR, self.pitch_shift)
+        if self.log_mel:
+            if self.LogMelSpect is None:
+                data = torchaudio.compliance.kaldi.fbank(
+                    data, htk_compat=True, sample_frequency=self.sample_rate,
+                    use_energy=False, window_type='hanning',
+                    num_mel_bins=self.n_mels, dither=0.0,
+                    frame_shift=self.frame_shift,frame_length=self.frame_length
+                )
+                data = data.unsqueeze(0)
+            else:
+                data = self.LogMelSpect(data)
+        if self.normalize and self.log_mel:
+            data = self.Normalize(data)
+        elif self.normalize:
+            data = (data - DATASET_RAW_MEAN) / DATASET_RAW_STD
+        #padding to target_len
+        if self.target_len is not None and self.log_mel and self.use_kaldi:
+            #source: AST transformer git repo
+            n_frames = data.shape[1]
+            p = self.target_len - n_frames
+            if p > 0:
+                m = torch.nn.ZeroPad2d((0, 0, 0, p))
+                data = m(data)
+            elif p < 0:
+                data = data[0:self.target_len, :]
+
+        return torch.squeeze(data)
+
+    def get_waveform(self, item: int):
+        """@returns the raw waveform data based on the index
+        @param[in]  item    index in the csv file
+        """
+        data, _ = torchaudio.load(self.path + "/audio/" + self.csv_data[item, 0])
+        return data
+
+    def _create_log_mel_spect(self):
+        """@returns an instance of nn.Sequential(transforms.MelSpectogram,
+                                                transforms.AmplitudeToDB)
+        """
+        to_melspect = torchaudio.transforms.MelSpectrogram(
+            sample_rate=self.sample_rate, n_fft=self.n_fft,
+            hop_length=self.hop_length, n_mels=self.n_mels)
+        to_db = torchaudio.transforms.AmplitudeToDB(stype="magnitude")
+
+        to_log_mel_spect = nn.Sequential(to_melspect, to_db)
+        if self.use_kaldi:
+            self.frame_shift=self.hop_length*1000/self.sample_rate
+            self.frame_length=self.n_fft*1000/self.sample_rate
+        return to_log_mel_spect if not self.use_kaldi else None
+
+    def _create_resample(self, original_sr, new_sr):
+        """@returns an instance of transforms.Resample"""
+        return torchaudio.transforms.Resample(
+            original_sr, new_sr)
+
+    #TODO: classes for augmentation and calling them with help of nn.Sequential
+
+    def inject_noise(self, data, max_noise_factor):
+        """@brief   adds white noise with random standard
+                    deviation to the data
+        @returns    the augmented data
+        @param[in]  max_noise_factor    max of the standard deviation
+        """
+        #random muliplier between 0 and max_noise_factor
+        noise_factor = torch.rand(1) * max_noise_factor
+        #white noise
+        noise = torch.randn(data.size(1))
+        augmented_data = data + noise_factor * noise
+        return augmented_data
+
+    def pitch_shift_rand(self, data, max_step):
+        """@brief   transforms the data using random pitch shift        
+        @returns    the augmented data
+        @param[in]  max_step    max of the n_steps parameter for functional.pitch_shift
+        """
+        #random n_steps
+        n_steps = np.random.randint(-max_step, max_step + 1)
+        augmented_data = torchaudio.functional.pitch_shift(
+            data, self.sample_rate, n_steps)
+        return augmented_data
+
+    def time_shift_rand(self, data, shift_max):
+        """@returns the augmented data
+        @param[in]  shift_max   max shift in seconds
+        """
+        #conversion from seconds
+        shift_max_steps = int(shift_max * self.sample_rate)
+        shift = np.random.randint(-shift_max_steps, shift_max_steps)
+        #shifting the data
+        augmented_data = torch.roll(data, shift)
+        if shift >= 0:
+            augmented_data[0, :shift] = 0
+        else:
+            augmented_data[0, shift:] = 0
+        return augmented_data
+
+    def standardize(self, data):
+        """@brief   dividing the data with the maximum value"""
+        max = torch.max(data)
+        return data/max
+
+    def _create_normalize(self):
+        """
+        @brief  returns an instance of torchvision.transforms.Normalize
+                with the previously calculated mean and std 
+        """
+        return Normalize(DATASET_KALDI_MEAN if self.use_kaldi else DATASET_MEAN,
+                        2 * DATASET_KALDI_STD if self.use_kaldi else DATASET_STD)
+
+    def transform(self, data, shift_max, max_step,
+                    max_noise_factor, pitch_shift=False):
+        """
+        @brief      applies time shift, pitch shift, noise injection
+        @returns    the augmented data
+        """
+        #data = standardize(data)
+        data = self.time_shift_rand(data, shift_max)
+        if pitch_shift:
+            data = self.pitch_shift_rand(data, max_step)
+        data = self.inject_noise(data, max_noise_factor)
+        return data
---
+++ trainer.py
@@ -0,0 +1,238 @@
+import copy
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+
+class Epoch_counter():
+    def __init__(self, max_batch):
+        self.epoch = 0
+        self.batch = 0
+        self.max_batch = max_batch
+        
+    def inc_epoch(self):
+        self.epoch += 1
+    
+    def get_epoch(self):
+        return self.epoch
+
+    def get_epoch_float(self):
+        return self.epoch + self.batch / self.max_batch
+
+    def inc_batch(self):
+        self.batch +=1
+
+    def get_batch(self):
+        return self.batch
+
+    def reset_batch(self):
+        self.batch = 0
+
+class Training_stats_logger():
+    def __init__(self, max_batch):
+        self.train_losses = []
+        self.last_train_loss_count = 0
+        self.last_train_loss_sum = 0
+        self.epoch_counter = Epoch_counter(max_batch)
+        self.last_train_stats = {
+                'val_loss': [],
+                'val_acc': [],
+                'avg_train_loss': [],
+                'epoch': []
+        }
+
+    def add_train_loss(self, loss):
+        self.train_losses.append(loss)
+        self.last_train_loss_count += 1
+        self.last_train_loss_sum += loss
+
+    def get_avg_loss(self):
+        return self.last_train_loss_sum / self.last_train_loss_count \
+            if 0 != self.last_train_loss_count else None
+
+    def reset_last_train_loss(self):
+        self.last_train_loss_count = 0
+        self.last_train_loss_sum = 0
+
+    def save_last_train_stats(self, val_loss, val_acc):
+        self.last_train_stats['val_loss'].append(val_loss)
+        self.last_train_stats['val_acc'].append(val_acc)
+        self.last_train_stats['avg_train_loss'].append(self.get_avg_loss())
+        self.last_train_stats['epoch'].append(self.epoch_counter.get_epoch_float())
+
+    def get_last_train_stats(self):
+        return self.last_train_stats
+
+    def print_log_message(self, val_loss, val_acc):
+        print(f"Avg train loss at {self.epoch_counter.get_epoch()}.epoch, "
+            f"{self.epoch_counter.get_batch()}. batch:\t"
+            f"{self.get_avg_loss():.3f}\t\t"
+            f"Val_loss at {self.epoch_counter.get_epoch()}.epoch, "
+            f"{self.epoch_counter.get_batch()}.batch:\t"
+            f"{val_loss:.3f}\tacc: {val_acc:.3f}")
+
+class Trainer():
+    """@brief class for training and validating models"""
+    def __init__(self, model, cuda_available, criterion=nn.CrossEntropyLoss,
+                log_message=True):
+        """@param[in]   model   deep learning model to train or validate
+        @param[in]  criterion   loss function for training and validation
+        @param[in]  cuda_available  if cuda is available
+        @param[in]  log_message     if log messages during training 
+        """
+        if cuda_available:
+            model = model.cuda()
+        self.model = model
+        self.criterion = criterion()
+        self.cuda_available = cuda_available
+        self.last_train_data = None
+        self.log_message = log_message 
+    
+    def train(self, train_loader, val_loader, optimizer=torch.optim.AdamW, lr=0.001,
+              train_epochs=30, val_interval=50, save_best_model=True,
+              env_save_path='.', saved_file_name='tmp.pth'):
+        """@brief   trains the model with the given parameters, saves the best model,
+            saves the training data for analysis
+        @param[in]  train_loader    dataloader for the training data
+        @param[in]  optimizer       optimizer for model training
+        @param[in]  lr      learning rate for the optimizer
+        @param[in]  train_epochs    duration of training
+        @param[in]  val_interval    num of batches after validation is done
+        @param[in]  log     bool, if logging is enabled
+        @param[in]  save_best_model bool, if saving the best model is required
+        @param[in]  env_save_path   save path to save the best model
+        @param[in]  saved_file_name name of the file the best model is saved to
+        TODO
+        """
+        #TODO
+        save_dest = env_save_path + '/' + saved_file_name
+
+        self.optimizer = optimizer(self.model.parameters(), lr=lr)
+        # for saving the best model
+        prevloss, _ = self.validate(val_loader)
+        minloss = prevloss
+        self.train_stats_logger = Training_stats_logger(len(train_loader))
+
+        for _ in range(train_epochs):
+            self._step_epoch(train_loader, val_loader, val_interval, minloss,
+                    save_best_model, save_dest)   
+        if self.log_message:
+            print('Finished Training')
+    
+    def _step_epoch(self, train_loader, val_loader, val_interval, minloss,
+                    save_best_model, save_dest):
+        """TODO"""
+        self.train_stats_logger.reset_last_train_loss()
+        self.train_stats_logger.epoch_counter.reset_batch()
+        self.train_stats_logger.epoch_counter.inc_epoch()
+
+        for inputs, labels in train_loader:
+            loss = self._step(inputs, labels)
+            # summing loss for plotting average
+            self.train_stats_logger.add_train_loss(loss)
+            self.train_stats_logger.epoch_counter.inc_batch()
+
+            if 0 == self.train_stats_logger.epoch_counter.get_batch() % val_interval:
+                # printing statistics
+                val_loss, val_accuracy = self.validate(val_loader)
+
+                if self.log_message:
+                    self.train_stats_logger.print_log_message(val_loss, val_accuracy)
+                self.train_stats_logger.save_last_train_stats(val_loss, val_accuracy)
+                self.train_stats_logger.reset_last_train_loss()
+
+                if val_loss < minloss and save_best_model:
+                    #saving the best model
+                    torch.save(
+                        {
+                            'epoch': self.train_stats_logger.epoch_counter.get_epoch_float(),
+                            'model': self.model.state_dict(),
+                            'optimizer': self.optimizer.state_dict()
+                        },
+                        save_dest)
+    
+    def _step(self, inputs, labels):
+        """TODO"""
+        if self.cuda_available:
+                inputs, labels = inputs.cuda(), labels.cuda()
+        self.optimizer.zero_grad()
+        self.model.train()
+
+        # forward pass
+        outputs = self.model(inputs)
+        # calculating loss
+        loss = self.criterion(outputs, labels)
+        # backpropagation
+        loss.backward()
+        self.optimizer.step()
+        return loss.item()
+
+    
+    def validate(self, val_loader):
+        """@brief   validate model on the dataset wrapped by loader
+        @returns    (validation loss, accuracy)
+        @param[in]  val_loader  DataLoader class wrapping the val dataset
+        """
+        self.model.eval()
+        val_loss = 0
+        total = 0
+        correct = 0
+        
+        #calculating the sum of the losses on the validation data
+        with torch.no_grad():
+            for inputs, labels in val_loader:
+                if self.cuda_available:
+                    inputs, labels = inputs.cuda(), labels.cuda()
+                outputs = self.model(inputs)
+                #calculating loss
+                loss = self.criterion(outputs, labels)
+                val_loss += loss.item()
+                #calculating accuracy
+                predictions = torch.argmax(outputs, 1)
+                total += labels.size(0)
+                correct += (predictions == labels).sum().item()
+        #returning the mean of the loss on the validation data
+        return val_loss/len(val_loader), correct/total
+    
+    def test(self, test_loader):
+        """@brief  tests the model on the test dataset
+        @returns   the accuracy on the test set
+        @param[in] test_loader DataLoader class wrapping the test dataset"""
+        _, test_acc = self.validate(test_loader)        
+        return test_acc
+
+    def plot_train_proc(self, title):
+        """@brief   plots validation and running data of the last training
+        @param[in]  title   title of the plotted figure"""
+        last_train_stats = self.train_stats_logger.get_last_train_stats()
+        epochs = last_train_stats['epoch']
+
+        fig, ax_loss = plt.subplots()
+        ax_acc = ax_loss.twinx()
+        ax_loss.plot(epochs, last_train_stats['val_loss'])
+        ax_loss.plot(epochs, last_train_stats['avg_train_loss'])
+        ax_acc.plot(epochs, last_train_stats['val_acc'])
+        
+        ax_loss.set_title(title)
+        ax_loss.set_xlabel('epoch')
+        ax_loss.set_ylabel('loss', color='g')
+        ax_acc.set_ylabel('accuracy', color='b')
+        plt.grid()
+        plt.show()
+                
+    def hyperparameter_plotting(self, lrs, train_loader, val_loader,
+        optimizer=torch.optim.AdamW, train_epochs=10, val_interval=10,
+        log=False):
+        """TODO"""
+        hyperparam_data = []
+        model_dict = copy.deepcopy(self.model.state_dict())
+        print(model_dict['module.mlp_head.1.weight'])
+        for lr in lrs:
+            self.train(train_loader, val_loader, optimizer=optimizer, lr=lr,
+                train_epochs=train_epochs, val_interval=val_interval, log=log,
+                save_best_model=False, log_train_data=True)
+            hyperparam_data.append((lr, self.last_train_data))
+            self.plot_train_proc(f'lr = {lr}')
+            self.model.load_state_dict(model_dict)
+            print(model_dict['module.mlp_head.1.weight'])
+            
+        return hyperparam_data
