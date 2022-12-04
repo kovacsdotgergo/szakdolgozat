cc907a0f7df2b91451123f4849d3d6769ffe4281
kovacsdotgergo <kovacsdotgergo@gmail.com>
2022-12-04 11:08:10 +0100

    new cnn initial commit

--- cnn.py
+++ cnn.py
@@ -50,4 +50,60 @@ class conv2d_v1(nn.Module):
 class cnn2d_v2(nn.Module):
     """TODO class for residual deep cnn with batchnorm"""
     def __init__(self):
-        raise NotImplementedError
+        super(cnn2d_v2, self).__init__()
+        self.cnn1 = Cnn_block(layer_num=3, in_channels=1, out_channels=16,
+            in_h=436, in_w=128)
+        self.pool1 = nn.MaxPool2d(2)
+        self.flatten = nn.Flatten(1)
+        self.mlp = torch.nn.Linear(in_features=16*218*64, out_features=50)
+    
+    def forward(self, input: torch.tensor) -> torch.tensor:
+        #unsqueeze if there is no batch dim
+        if 3 == input.dim():
+            input = input.unsqueeze(dim=0)
+        input = self.cnn1(input)
+        input = self.pool1(input)
+        print(input.size())
+        input = self.flatten(input)
+        print(input.size())
+        input = self.mlp(input)
+        return input
+
+class Cnn_block(nn.Module):
+    def __init__(self, layer_num, in_channels, out_channels, in_h, in_w):
+        super(Cnn_block, self).__init__()
+        self.layer_num = layer_num
+        self.cnns = []
+        self.layer_norms = []
+        for i in range(layer_num):
+            self.cnns.append(nn.Conv2d(
+                in_channels=in_channels if i == 0 else out_channels,
+                out_channels=out_channels,
+                kernel_size=(3, 3),
+                stride=(1, 1),
+                padding=(1, 1)))
+            self.layer_norms.append(
+                nn.LayerNorm(normalized_shape=[out_channels, in_h, in_w]))
+            
+        if in_channels != out_channels:
+            self.project_residual = nn.Conv2d(in_channels=in_channels,
+                                          out_channels=out_channels,
+                                          kernel_size=(1, 1),
+                                          stride=(1, 1))
+        else:
+            self.project_residual = None
+        self.relu = nn.ReLU()
+    
+    def forward(self, input:torch.tensor) -> torch.tensor:        
+        cnn_input = input.clone()
+        for i, (cnn_layer, norm_layer) in enumerate(zip(self.cnns, self.layer_norms)):
+            cnn_input = cnn_layer(cnn_input)
+            cnn_input = norm_layer(cnn_input)
+            if i < self.layer_num - 1:
+                cnn_input = self.relu(cnn_input)
+        if self.project_residual is not None: 
+            input = self.project_residual(input)
+        output = cnn_input + input
+        output = self.relu(output)
+        return output
+        
